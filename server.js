aa9e9ff37e625efddb4b2fd64c9a37408887390d
praiselin95 <30790886+praiselin95@users.noreply.github.com>
2017-08-09 12:03:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,9 +5,52 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
+var person={
+    name:'jerk',
+    age:'24',
+    dateOfBirth:'May 11,1994'
+}
+
+function createTemplate(data)
+{
+    var name=data.name;
+     var age=data.age;
+      var dateOfBirth=data.dateOfBirth;
+   
+   var htmlTemplate=`
+   
+   <html>
+   
+   <head>
+   <title>
+   ${name}
+   </title>
+   
+   </head>
+   
+   <body>
+   <div class="container">
+   <h3>${name}<h3>
+   <div>${age}</div>
+   <div>${dateOfBirth}</div>
+   
+   
+   
+   </div>
+   
+   </body>
+   </html>`
+   return htmlTemplate;
+   
+   
+   
+}
+
+
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
+
 
 app.get('/new-file', function (req, res) {
   res.send('New file got created');
