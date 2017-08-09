c7b5ab7071f0a01d46e717fa3a1ca99ccd1955b6
praiselin95 <30790886+praiselin95@users.noreply.github.com>
2017-08-09 13:19:46 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,5 +1,5 @@
 console.log('Loaded!');
-var buttom=document.getElementById('counter');
+var button=document.getElementById('counter');
 button.onclick=function(){
     var request=new XMLHttpRequest();
     request.onreadystatechange=function(){
