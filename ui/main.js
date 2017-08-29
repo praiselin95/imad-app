da975a865750923087b9fa4e1794795530c1b36e
praiselin95 <30790886+praiselin95@users.noreply.github.com>
2017-08-29 10:36:45 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,7 +1,17 @@
 console.log('Loaded!');
-var username=document.getElementById('username');
-var password=document.getElementById('password');
-var submit=document.getElementById('submit')
+  var data = {};
+data.username=document.getElementById('username');
+data.password=document.getElementById('password');
+$.ajax({
+						type: 'POST',
+						data: JSON.stringify(data),
+				        contentType: 'application/json',
+                        url:'http://praiselinvictor.imad.hasura-app.io/create-user',						
+                        success: function(data) {
+                            console.log('success');
+                            console.log(JSON.stringify(data));
+                        }
+                    });
 
 /*var button=document.getElementById('counter');*/
 /*button.onclick=function(){
@@ -15,18 +25,12 @@ var submit=document.getElementById('submit')
             }
         }
     };*/
-    submit.onclick=function(){
-    var request=new XMLHttpRequest();
-    request.onreadystatechange=function(){
-        if(request.readyState===XMLHttpRequest.DONE){
-            if(request.status===200){
-                var username=request.responseText;
-                 var password=request.responseText;
-            }
-        }
-    };
+    
    /* request.open('GET','http://praiselinvictor.imad.hasura-app.io/counter',true);*/
-    request.open('GET','http://praiselinvictor.imad.hasura-app.io/create-user',true);
+ 
+				
+   
+    /*request.open('GET','http://praiselinvictor.imad.hasura-app.io/create-user',true);*/
     
   
     request.send(null);
