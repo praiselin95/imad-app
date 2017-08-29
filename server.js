6d0c696cb72325203b5342293fcd96ac5f2925ab
praiselin95 <30790886+praiselin95@users.noreply.github.com>
2017-08-29 11:03:26 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -78,7 +78,7 @@ function hash(input,salt)
 {
     var hashed=crypto.pbkdf2Sync('secret', 'salt', 100000, 512, 'sha512');
     // return hashed.toString('hex');
-     return ["pbkdf2","10000",salt,key.toString('hex')].join('$');
+     return ["pbkdf2","10000",salt,hashed.toString('hex')].join('$');
 }
 
 app.get('/hash/:input', function (req, res) {
